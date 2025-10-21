e24c03f1800ec16c49df123792707f27c7e5ae9a
CA Harsh Agrawal <pwctech@Harshs-MacBook-Pro.local>
2025-10-21 11:17:26 +0530

    fix: removed rate limit to get_reviews

--- lms/lms/utils.py
+++ lms/lms/utils.py
@@ -246,7 +246,6 @@ def get_average_rating(course):
 
 
 @frappe.whitelist(allow_guest=True)
-@rate_limit(limit=100, seconds=60 * 60)
 def get_reviews(course):
 	reviews = frappe.get_all(
 		"LMS Course Review",
