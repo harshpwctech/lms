edc6560e9b6a9a55d8cbd9e8781153560f9173b7
CA Harsh Agrawal <pwctech@Harshs-MacBook-Pro.local>
2025-10-28 11:32:01 +0530

    fix: used LMS Certificate Doc for custom email template

--- lms/lms/doctype/lms_certificate/lms_certificate.py
+++ lms/lms/doctype/lms_certificate/lms_certificate.py
@@ -40,7 +40,7 @@ class LMSCertificate(Document):
 		}
 
 		if custom_template:
-			email_template = get_email_template(custom_template, self)
+			email_template = get_email_template(custom_template, self.as_json())
 			subject = email_template.get("subject")
 			content = email_template.get("message")
 		frappe.sendmail(
